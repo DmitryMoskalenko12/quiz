fe24f458a4950b9df586541d3fb6c265dec4c3bf
Dima Moskalenko <dmtrmsklnk@gmail.com>
2024-03-03 13:53:28 +0200

    finish

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,8 @@
-import { useState, useCallback, useRef } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from '../questions';
 import quizCompleteImg from '../assets/quiz-complete.png';
 import Question from "./Question";
+import Summary from "./Summary";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
@@ -19,10 +20,7 @@ export default function Quiz() {
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (quizIsComplete) {
-      return <div id="summary">
-              <img src={quizCompleteImg} alt="Trophy icon" />
-              <h2>Quiz Complete!</h2>
-             </div>
+      return <Summary userAnswers={userAnswers}/>
     }
 
     return (
